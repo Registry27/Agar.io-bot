7197734d13cfae07c6be9088e5cda401bee932a7
Registry27 <kalbim-yamab27@hotmail.com>
2015-07-21 16:08:20 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        AposLauncher
-// @namespace   AposLauncher
-// @include     http://agar.io/*
+// @name        İBOREYİZ
+// @namespace   İBOREYİZ
+// @include     http://agario.volismt2.com/*
 // @version     3.052
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
